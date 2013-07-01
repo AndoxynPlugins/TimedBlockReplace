b0e41a2210ec2aea0d6c3948774c3c03be09ada3
Dabo Ross <dabodialga@gmail.com>
2013-07-01 01:17:43 -0700

    Implemented remove command

--- src/main/java/net/daboross/bukkitdev/timedblockreplace/subcommandhandlers/RemoveCommandHandler.java
+++ src/main/java/net/daboross/bukkitdev/timedblockreplace/subcommandhandlers/RemoveCommandHandler.java
@@ -5,9 +5,12 @@
  */
 package net.daboross.bukkitdev.timedblockreplace.subcommandhandlers;
 
+import java.util.logging.Level;
 import net.daboross.bukkitdev.timedblockreplace.TimedBlockReplace;
+import net.daboross.bukkitdev.timedblockreplace.commandexecutorbase.ColorList;
 import net.daboross.bukkitdev.timedblockreplace.commandexecutorbase.SubCommand;
 import net.daboross.bukkitdev.timedblockreplace.commandexecutorbase.SubCommandHandler;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -25,6 +28,30 @@ public class RemoveCommandHandler implements SubCommandHandler {
 
     @Override
     public void runCommand(CommandSender sender, Command baseCommand, String baseCommandLabel, SubCommand subCommand, String subCommandLabel, String[] subCommandArgs) {
-        sender.sendMessage("Derp! Derp Derp Derp! Derp Derp ,asdfaweinullnm Derp!");
+        if (subCommandArgs.length < 1) {
+            sender.sendMessage(ColorList.ERR + "Please specify a block record to remove!");
+            sender.sendMessage(subCommand.getHelpMessage(baseCommandLabel, subCommandLabel));
+            return;
+        }
+        int fromBlockID;
+        try {
+            fromBlockID = Integer.parseInt(subCommandArgs[0]);
+        } catch (NumberFormatException nfe) {
+            sender.sendMessage(ColorList.ERR_ARGS + subCommandArgs[0] + ColorList.ERR + " is not an integer! Please use the block ID, not the block name!");
+            sender.sendMessage(subCommand.getHelpMessage(baseCommandLabel, subCommandLabel));
+            return;
+        }
+        if (fromBlockID <= 0) {
+            sender.sendMessage(ColorList.ERR_ARGS + fromBlockID + ColorList.ERR + " is not a non-0 positive integer! Please use the block ID, not the block name!");
+            sender.sendMessage(subCommand.getHelpMessage(baseCommandLabel, subCommandLabel));
+            return;
+        }
+        tbr.getLogger().log(Level.INFO, "Removing Record: fromBlockID={0}", fromBlockID);
+        if (tbr.removeConfigBlock(fromBlockID)) {
+            sender.sendMessage(ColorList.REG + "Record for " + ColorList.DATA + fromBlockID + ColorList.REG + " has been removed!");
+        } else {
+            sender.sendMessage(ColorList.REG + "Record for " + ColorList.DATA + fromBlockID + ColorList.REG + " didn't exist!");
+        }
+        sender.sendMessage(ColorList.DATA + fromBlockID + ColorList.REG + "'s name is: " + Material.getMaterial(fromBlockID));
     }
 }
